27776e2d5e2ee504355475450f7ac0a22f7b68b9
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2025-03-10 13:34:43 +0100

    [init]  | Files: .gitignore, Api/.gitignore, Api/README.md, Api/package-lock.json, Api/package.json, Api/src/app.js, Api/src/server.js, docker-compose.yml, docs/API.md

---
+++ Api/src/app.js
@@ -0,0 +1,4 @@
+﻿import express from 'express';
+const app = express();
+app.use(express.json());
+export default app;
---
+++ Api/src/server.js
@@ -0,0 +1,7 @@
+﻿import express from 'express';
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listens on http://localhost:' + port + '/api'));
