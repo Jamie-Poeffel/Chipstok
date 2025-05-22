9e5e405d011c1e6e0f08d84de8e914080bdea5f1
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2025-05-22 17:40:35 +0200

    Remove deprecated files and controllers related to authentication, OAuth, posts, users, and services. This includes multer configuration, auth middleware, and various route handlers, streamlining the API structure and improving maintainability.

--- Api/dist/routes/oauthRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const oauthController_1 = require("../controllers/oauthController");
-const router = (0, express_1.Router)();
-router.get('/authorize', oauthController_1.Authorize);
-exports.default = router;
