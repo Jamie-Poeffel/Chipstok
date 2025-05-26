edb831174b371926e48ad4b9d5558049fe43dd66
Alessio-Huber <alessio.huber@hotmail.com>
2025-05-26 12:59:56 +0200

    [fix] deleted unnecessary code | Files: Chipsytok/src/views/SignupView.vue

--- Chipsytok/src/views/SignupView.vue
+++ Chipsytok/src/views/SignupView.vue
@@ -176,10 +176,6 @@ onMounted(() => {
           </button>
         </div>
       </div>
-      <div>
-        <button @click="showLanguageModal = true" aria-label="Choose language"
-          class="mt-4 mx-auto block text-gray-500 hover:text-gray-700"></button>
-      </div>
     </div>
   </div>
 </template>
